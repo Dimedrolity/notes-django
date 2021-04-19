ff203accf3f980785e60116d899efb64be76dab4
Dimedrolity <dima.redkin.1999@gmail.com>
2021-04-19 12:32:51 +0500

    note model

--- my_notes/notes/models.py
+++ my_notes/notes/models.py
@@ -3,3 +3,13 @@ from django.db import models
 # Create your models here.
 
 
+class Note(models.Model):
+    title = models.TextField(verbose_name="Заголовок", default='', blank=True)
+    text = models.TextField(verbose_name="Основной текст", default='', blank=True)
+
+    class Meta:
+        verbose_name = 'Заметка'
+        verbose_name_plural = 'Заметки'
+
+    def __str__(self):
+        return self.title
